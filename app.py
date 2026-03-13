5e4eb5d529e585c1ccddc169786549eb0c1a805e
AkhilaRao0002 <akhilaraod@gmail.com>
2026-03-14 00:38:24 +0530

    Refactor import statements and clean up code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # =============================
 # Load saved files
