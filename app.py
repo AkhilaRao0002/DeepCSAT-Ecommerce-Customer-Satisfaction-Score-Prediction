55244c376188fc484856a813aabafe59bfa3e554
AkhilaRao0002 <akhilaraod@gmail.com>
2026-03-14 21:18:54 +0530

    Update model loading and fix file name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tensorflow.keras.models import load_model
 # =============================
 # Load saved files
 # =============================
-model = load_model("csat_ann_model.keras")
+model = load_model("csat_ann_model_fixed.h5", compile=False)
 scaler = joblib.load("scaler.pkl")
 training_columns = joblib.load("training_columns.pkl")
 
